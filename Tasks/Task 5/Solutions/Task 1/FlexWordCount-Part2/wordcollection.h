d839d883ca11eb7c4e949e2d58a833cd67866cdb
James Marino <jamesmarinoau@gmail.com>
2015-10-24 13:23:01 +1100

    Task 5 Part one done - Use absolute file address for filename

---
+++ Tasks/Task 5/Solutions/Task 1/FlexWordCount-Part2/wordcollection.h
@@ -0,0 +1,26 @@
+#ifndef WORDCOLLECTION_H
+#define	WORDCOLLECTION_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    struct word {
+        char* _str;
+        int _count;
+    };
+    
+    typedef struct word Word;
+    
+    int getNumWords();
+    
+    Word* getWord(int ndx);
+    
+    void addWord(char* astr);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* WORDCOLLECTION_H */
+
